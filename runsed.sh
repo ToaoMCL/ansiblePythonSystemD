4de5a40796cd4ae71d6a004756faaa8dfd482769
Eryk Pabianczyk <65418606+ToaoMCL@users.noreply.github.com>
2023-07-17 13:54:08 +0100

    Create runsed.sh

---
+++ runsed.sh
@@ -0,0 +1,15 @@
+sed -i 's/localhost/all/g' playbook.yaml
+sed -i 's/172\.31\.44\.207/54.78.97.167/g' inventory.yaml
+sed -i 's#/home/ubuntu/.ssh/myKey#/home/jenkins/eu-west-1-personal.pem#g' inventory.yaml
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                                                                                                                                                                                           
+~                              
